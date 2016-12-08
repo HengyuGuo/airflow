dec818c61f93e6c79f5770f446092c1e89ab8f4a
Allan Stewart <astewart@summitps.org>
2016-12-07 19:35:02 -0800

    Initial commit

---
+++ dags/test_redshift.py
@@ -0,0 +1,35 @@
+"""
+Code that goes along with the Airflow located at:
+http://airflow.readthedocs.org/en/latest/tutorial.html
+"""
+from airflow import DAG
+from airflow.operators.postgres_operator import PostgresOperator
+from datetime import datetime, timedelta
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2015, 6, 1),
+    'email': ['astewart@summitps.org'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG(
+    'test_redshift', default_args=default_args, schedule_interval='0 * * * *')
+
+t1 = PostgresOperator(
+  task_id='count_table_rows',
+  sql="""
+      SELECT COUNT(1) FROM heroku_public.districts
+  """,
+  postgres_conn_id='redshift',
+  dag=dag,
+)
---
+++ dags/test_snowflake.py
@@ -0,0 +1,35 @@
+"""
+Code that goes along with the Airflow located at:
+http://airflow.readthedocs.org/en/latest/tutorial.html
+"""
+from airflow import DAG
+from airflow.operators.jdbc_operator import JdbcOperator
+from datetime import datetime, timedelta
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2015, 6, 1),
+    'email': ['airflow@airflow.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG(
+    'test_snowflake', default_args=default_args, schedule_interval='0 * * * *')
+
+t1 = JdbcOperator(
+  task_id='count_table_rows',
+  sql="""
+      SELECT COUNT(1) FROM districts
+  """,
+  jdbc_conn_id='snowflake_test_db',
+  dag=dag,
+)
