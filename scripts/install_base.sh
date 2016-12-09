e426c3605aeced74ccc61fe3b0b63531b62bb37e
Allan Stewart <astewart@summitps.org>
2016-12-08 16:11:21 -0800

    Update script comment

--- scripts/install_base.sh
+++ scripts/install_base.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Note: On linux you might need to run with sudo.
+# You may also need to run sudo apt-get build-dep python-psycopg2
+
 pip install airflow
 pip install 'airflow[postgres, s3, jdbc, celery]'
 echo 'export AIRFLOW_HOME=~/airflow' >> ~/.bash_profile
