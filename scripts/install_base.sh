59fe5f2f28fefc7f69c593a4e5297605a815f29d
Allan Stewart <astewart@summitps.org>
2017-01-17 12:03:22 -0800

    Add gcp_api

--- scripts/install_base.sh
+++ scripts/install_base.sh
@@ -7,7 +7,7 @@
 
 pip install airflow
 pip install JayDeBeApi==0.2.0
-pip install 'airflow[postgres, s3, jdbc, celery, hive]'
+pip install 'airflow[postgres, s3, jdbc, celery, hive, gcp_api]'
 pip install cryptography
 echo 'export AIRFLOW_HOME=$HOME/airflow' >> ~/.bash_profile
 echo 'export CLASSPATH="$CLASSPATH:$HOME/airflow/java/snowflake-jdbc-3.0.3.jar"' >> ~/.bash_profile
